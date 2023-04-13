5b2aa6824c5120d8f131046a558c11cf195b91cc
nataha11 <79098507+nataha11@users.noreply.github.com>
2023-04-13 21:58:59 +0300

    comparing conversions

--- solution.py
+++ solution.py
@@ -8,7 +8,5 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    return (x_success/x_cnt < y_success/y_cnt) # Ваш ответ, True или False
